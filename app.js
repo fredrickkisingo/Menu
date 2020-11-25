02222b9c953b63883e2b2660226abfa24082c811
fred <fredrickkisingo@gmail.com>
2020-11-25 20:42:16 +0300

    changes to the menu descriptions

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const menu = [
     category: "breakfast",
     price: 15.99,
     img: "./images/item-1.jpeg",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: `Fresh pancakes made with butterhoney and milk.Get strawberry toppings for free`,
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const menu = [
     category: "lunch",
     price: 13.99,
     img: "./images/item-2.jpeg",
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    desc: `A double lunch for you the American way.Get your double combo deal at a highly affordable price from our cafe`,
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ const menu = [
     category: "shakes",
     price: 6.99,
     img: "./images/item-3.jpeg",
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    desc: `Tasty delicious milkshakes available in flavors of strawberry,chocolate and vanilla.Get your milkshake from your favorite Joint!`,
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ const menu = [
     category: "breakfast",
     price: 20.99,
     img: "./images/item-4.jpeg",
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    desc: `A breakfast befitting for your hunger.Get freshly cooked breakfast from here`,
   },
   {
     id: 5,
@@ -37,7 +37,7 @@ const menu = [
     category: "lunch",
     price: 22.99,
     img: "./images/item-5.jpeg",
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    desc: `Eggs just the way you love them`,
   },
   {
     id: 6,
@@ -45,7 +45,7 @@ const menu = [
     category: "shakes",
     price: 18.99,
     img: "./images/item-6.jpeg",
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    desc: `A sensational blend of Oreo and a chocolate shake from our cafe.Feel free to order`,
   },
   {
     id: 7,
@@ -53,7 +53,7 @@ const menu = [
     category: "breakfast",
     price: 8.99,
     img: "./images/item-7.jpeg",
-    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    desc: `A bacon dish freshly cooked `,
   },
   {
     id: 8,
@@ -61,7 +61,7 @@ const menu = [
     category: "lunch",
     price: 12.99,
     img: "./images/item-8.jpeg",
-    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    desc: `The perfect snack combo of french fries and a Large Burger.Get it at your convenience with free sauce of your choice `,
   },
   {
     id: 9,
@@ -69,7 +69,7 @@ const menu = [
     category: "shakes",
     price: 16.99,
     img: "./images/item-9.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    desc: `For you and your better Half`,
   },
   {
     id: 10,
@@ -77,7 +77,7 @@ const menu = [
     category: "dinner",
     price: 38.99,
     img: "./images/item-10.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    desc: `A mouth watering steak`,
   },
 ];
 
